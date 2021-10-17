2cf1b70c11f55db5c49a52e335dde9239463e509
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2021-10-17 11:30:32 +0600

    Done with this

---
+++ src/components/Player/Player.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Player = () => {
+    return (
+        <div>
+            <h1>I am a Boom Boom player</h1>
+        </div>
+    );
+};
+
+export default Player;
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import Player from '../Player/Player';
 
 const User = (props) => {
     const {familiar} = props;
 
-c
+    // 1. Element Variable
+    let greeting;
+    if(familiar){
+        greeting = <h3>Good Morning to you my dear.</h3>
+    }
+    else{
+        greeting = <h3>Ow! you are not familar with this?</h3>
+    }
+
+    return (
+        <div>
+            {greeting}
+
+            {/* 2. Ternary Operator */}
+            {familiar ? <h3>Good Morning to you my dear.</h3> : <h3>Ow! you are not familar with this?</h3>}
+            <div>
+                <h2>Yo Yo chiki chiki</h2>
+                {
+                    familiar 
+                    ? 
+                    <div>
+                        <h3>This is a heading</h3>
+                        <p>Tui to amar bondhu</p>
+                    </div>
+                    :
+                    <div>
+                        <h3>Kire mama, tore to chinlam na</h3>
+                        <p>Tui to amar bondhu na</p>
+                    </div>
+                }
+            </div>
+
+            <div>
+                <h2>Connection</h2>
+                {
+                    familiar && <div>
+                        <h5>Dosto phone number dao</h5>
+                    </div>
+                }
+            </div>
+            <div>
+                <h2>Shared Drink</h2>
+                {
+                    familiar || <h4>Khamu daab er pani, haramu sob khani</h4>
+                }
+            </div>
+            <div>
+                <h1>Player</h1>
+                {
+                    familiar && <Player></Player>
+                }
+            </div>
+        </div>
+    );
 };
 
 export default User;
